ac97c4ed61eda52a46c65d7d56ea8d4554df44ff
Mattias <duartemattias3@gmail.com>
2023-06-15 18:27:13 -0400

    adding reset css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Navigation } from "./router/Navigation";
 const App = () => {
     return (
         <BrowserRouter>
-            <Navigation/>       
+            <Navigation/>
         </BrowserRouter>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { darkTheme } from './themes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={ darkTheme }>
+      <CssBaseline/>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
