8255268ed5b2399b55bf2045eb2f4963e456ae6a
Mattias <duartemattias3@gmail.com>
2023-06-26 11:21:53 -0400

    Supabase conection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navigation } from "./router/Navigation";
+import { supabase } from "./supabase";
 
 
 const App = () => {
+    console.log(supabase);
+
     return (
         <BrowserRouter>
             <Navigation/>
